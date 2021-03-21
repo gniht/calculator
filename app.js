8dfe355770d437d7cbea2c9292c27b3d84467780
Jason Thomas <jasonthomasjat@gmail.com>
2021-03-20 20:52:23 -0700

    clean up

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 let currentEntry = document.querySelector('.currentEntry');
 let previouslyEntered = document.querySelector('.previouslyEntered');
 let bezel = document.querySelector('.bezel');
@@ -9,9 +8,6 @@ let previously = [];
 let lastTypeEntered = '';
 let fallbackValues = [];
 let openParens = false;
-
-
-
 
 bezel.addEventListener('click', (e) => {
   e.preventDefault();
@@ -25,14 +21,6 @@ bezel.addEventListener('click', (e) => {
   applyInput(keyPushed);
 });
 
-/*
-  1. get user input
-  2. discern what type of input the user entered
-     this falls into 5 general classes:
-     number, operator, delimiter, transform, clear
-  3. perform the appropriate action based on the input
-*/
-
 function applyInput(key){
   let num = parseFloat(current);  
 
@@ -41,8 +29,7 @@ function applyInput(key){
     current = temp[1];      
     previously = temp[0];
     lastTypeEntered = temp[2];
-    openParens = temp[3];
-    console.log('previously: '+ temp[0] + ' || current: ' + temp[1]);
+    openParens = temp[3];    
     previouslyEntered.textContent = previously.join(' ');
     currentEntry.textContent = current;    
   }else{
@@ -104,75 +91,17 @@ function applyInput(key){
   if( key.classList.contains('inverse') && num && num !== 0){
     current = '' + (1/num);
     currentEntry.textContent = current;   
-  }  
-}
-
-/*
-function doTheMath(strArr){  
-  if(strArr.length === 1){
-    return strArr[0];
   }
-  if(strArr.length === 3){
-    return doTheMath([operate(strArr)]);    
+  if( key.classList.contains('square') && num ){
+    current = '' + (num*num);
+    currentEntry.textContent = current;
   }
-  console.log('before: ' + strArr);  
-  // parse the str, do the math
-  // check for parens, process what's in them first
-  // since nested parens aren't supported, open and closed will be in pairs
-  let startParens = strArr.indexOf('(');
-  let endParens = strArr.indexOf(')');
-  
-  if( startParens >= 0 ){
-    let subArr = [];
-    for(let i = startParens+1; i < endParens; i++){
-      subArr.push(strArr[i]);
-    }
-    strArr.splice(startParens, endParens-startParens+1, doTheMath(subArr));    
-    doTheMath(strArr);
-  }
-  //multiply and divide  
-  let multIndex = strArr.indexOf('*');
-  let divIndex = strArr.indexOf('/');
-  let addIndex = strArr.indexOf('+');
-  let subtractIndex = strArr.indexOf('-');
-  let operationIndex;
-  while(multIndex >= 1 || divIndex >= 1){
-    if(multIndex >= 1 && divIndex >= 1){
-      operationIndex = Math.min(multIndex, divIndex);
-    }else if(divIndex < 0){
-      operationIndex = multIndex;
-    }else if(multIndex < 0){
-      operationIndex = divIndex;
-    }
-    strArr.splice(operationIndex-1, 3, doTheMath([
-      strArr[operationIndex-1],
-      strArr[operationIndex],
-      strArr[operationIndex+1]
-    ]));
-  }
-  console.log('after: ' + strArr); 
-
-}
-
-function operate(arr){
-  let a = parseFloat(arr[0]);
-  let b = parseFloat(arr[2]);
-  if( arr[1] === '*' ){
-    return a*b;
-  }
-  if( arr[1] === '/' ){
-    if( b === 0 ){
-      alert('To tally the magnitude of your error would take an eternity.');
+  if( key.classList.contains('sqrt') && num && num > 0 ){
+    if(num < 0){
+      alert('Cannot take sqrt of a negative number');
     }else{
-      return a/b;
-    }     
-  }
-  if( arr[1] === '+' ){
-    return a+b;
-  }
-  if( arr[1] === '-' ){
-    return a-b;
-  }
+      current = '' + Math.sqrt(num);
+      currentEntry.textContent = current;
+    }
+  }    
 }
-console.log('final: ' + doTheMath(['5', '*', '(', '3', '-', '2', ')', '+', '1']) );
-*/
